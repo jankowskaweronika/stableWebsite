c84240aac4520ae52e4111fad8c983cb161416f4
Weronika Jankowska <wjankowska8@gmail.com>
2024-04-04 12:59:08 +0200

    Merge branch 'origin/main'

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -28,10 +28,10 @@ const Features = () => {
               <div key={feature.name} className="relative">
                 <dt>
                   <div
-                    className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-primary border-4`}
+                    className={`absolute flex items-center justify-center h-15 w-20 rounded-md bg-background text-tertiary`}
                   >
                     <img
-                      className={`inline-block h-6 w-6 rounded-full`}
+                      className={`inline-block h-20 w-20 object-contain`}
                       src={feature.icon}
                       alt={feature.name}
                     />

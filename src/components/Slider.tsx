822a0adad9f22e39aaddc9457464943778eab35c
Weronika Jankowska <wjankowska8@gmail.com>
2024-04-04 16:20:44 +0200

    ✨ added Slider with random images

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,22 +1,126 @@
-import React from 'react';
+import * as React from 'react';
 
-import config from '../config/index.json';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import { useTheme } from '@mui/material/styles';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 
-const Pricing = () => {
-  const { pricing } = config;
-  const { title } = pricing;
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'San Francisco Oakland Bay Bridge, United States',
+    imgPath:
+      'https://v.wpimg.pl/aWNhZ28uTjYvCiwBFw5DI2xSeFtRV011O0pgEBdNUWc2Rz4cTwUIOilGKl4XTFhkf1pqAQ1YUG93Wm8DC0RXbmELMltbFAY4YAIqVRoI',
+  },
+  {
+    label: 'San Francisco Oakland Bay Bridge, United States',
+    imgPath:
+      'https://v.wpimg.pl/aWNhZ28uTjYvCiwBFw5DI2xSeFtRV011O0pgEBdNUWc2Rz4cTwUIOilGKl4XTFhkf1pqAQ1YUG93Wm8DC0RXbmELMltbFAY4YAIqVRoI',
+  },
+  {
+    label: 'San Francisco Oakland Bay Bridge, United States',
+    imgPath:
+      'https://v.wpimg.pl/aWNhZ28uTjYvCiwBFw5DI2xSeFtRV011O0pgEBdNUWc2Rz4cTwUIOilGKl4XTFhkf1pqAQ1YUG93Wm8DC0RXbmELMltbFAY4YAIqVRoI',
+  },
+  {
+    label: 'San Francisco Oakland Bay Bridge, United States',
+    imgPath:
+      'https://v.wpimg.pl/aWNhZ28uTjYvCiwBFw5DI2xSeFtRV011O0pgEBdNUWc2Rz4cTwUIOilGKl4XTFhkf1pqAQ1YUG93Wm8DC0RXbmELMltbFAY4YAIqVRoI',
+  },
+];
+
+function SwipeableTextMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step: number) => {
+    setActiveStep(step);
+  };
 
   return (
-    <section className={`bg-background py-8`} id="pricing">
-      <div className={`container mx-auto px-2 pt-4 pb-12 text-primary`}>
-        <h1
-          className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
-        >
-          {title}
-        </h1>
-      </div>
-    </section>
+    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      ></Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 255,
+                  display: 'block',
+                  maxWidth: 400,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
   );
-};
+}
 
-export default Pricing;
+export default SwipeableTextMobileStepper;

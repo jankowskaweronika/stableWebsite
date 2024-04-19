c2962ab9b2c65dfb0e6672842c348d0e2743d534
Weronika Jankowska <wjankowska8@gmail.com>
2024-04-19 17:38:48 +0200

    🚧  on contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -10,19 +10,6 @@ import Divider from './Divider';
 const Contact = () => {
   const { contact } = config;
   const { socialMedia } = contact;
-  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 768);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsWideScreen(window.innerWidth > 768);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
 
   return (
     <div
@@ -43,8 +30,8 @@ const Contact = () => {
           ))}
         </h1>
         <Divider />
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div>
+        <div className="grid grid-cols-3 gap-4">
+          <div className="col-span-2">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2506.0801094366384!2d22.34569511191108!3d51.08852907160298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4722f16e619fd6ff%3A0x9dbf1152ee612fc!2sStajnia%20Z%C5%82ota%20Podkowa!5e0!3m2!1spl!2spl!4v1712252138066!5m2!1spl!2spl"
               width="600"
@@ -52,14 +39,14 @@ const Contact = () => {
               loading="lazy"
             ></iframe>
           </div>
-          {isWideScreen && ( // Warunkowe renderowanie - jeśli szerokość ekranu jest większa niż 768px, renderuj Typography obok mapy
-            <div>
-              <Typography variant="h5">{contact.title2}</Typography>
-              <Typography variant="h6" margin={2}>
-                {contact.information}
-              </Typography>
-              <div>
-                <Typography variant="h5">{contact.title3}</Typography>
+          <div className="col-span-1">
+            <Typography variant="h5">{contact.title2}</Typography>
+            <Typography variant="h6" margin={2}>
+              {contact.information}
+            </Typography>
+            <div className="">
+              <Typography variant="h5">{contact.title3}</Typography>
+              <div className="m-3">
                 <a
                   aria-label="facebook"
                   href={socialMedia.facebook}
@@ -88,7 +75,7 @@ const Contact = () => {
                 </a>
               </div>
             </div>
-          )}
+          </div>
         </div>
         <div className="flex items-center mt-6">
           <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
@@ -99,5 +86,4 @@ const Contact = () => {
     </div>
   );
 };
-
 export default Contact;
